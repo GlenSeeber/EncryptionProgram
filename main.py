f63a5f6c71d887a936961443ccb54f3d293e1bf3
Glen Seeber <gseeber5@gmail.com>
2022-06-07 12:01:30 -0700

    make it work It will ask for directory, input file name, and the operation (encrypt or decrypt). I made the functions work, they will take the file, do the operation, and create a unique file to put the content onto. I also made a function that easily creates a unique file, pasting any content to it that you want.

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 from cryptography.fernet import Fernet
 
-def encrypt(myPath):
+
+
+def encrypt(myPath, fileName):
     #read a file
-    with open(myPath, 'rb') as f:
+    with open(myPath+fileName, 'rb') as f:
         inData = f.read()
 
     outData = fernet.encrypt(inData)
 
-    #write to a file
-    with open(myPath, 'wb') as f:
-        f.write(outData)
+    #create unique file
+    writeNewFile(myPath, 'encrypted-output', content=outData)
 
-def decrypt(myPath):
+
+def decrypt(myPath, fileName):
     #read a file
-    with open(myPath, 'rb') as f:
+    with open(myPath+fileName, 'rb') as f:
         inData = f.read()
 
     outData = fernet.decrypt(inData)
 
     #write to a file
-    with open(myPath, 'wb') as f:
-        f.write(outData)
+    writeNewFile(myPath, 'decrypted-output', content=outData)
 
+
+#creates a new file with a unique name, and writes content to it
+def writeNewFile(myPath, fileName, extension='.zip', flags='b', content=''):
+    n = -1
+    suffix = ''
+    running = True
+    while running:
+        try:
+            fullPath = myPath+fileName+suffix+extension
+            with open(fullPath, 'x'+flags) as f:
+                f.write(content)
+
+                running = False
+                #the name of the full directory + file name
+                return fullPath
+
+        except:
+            #increase the n counter until we get a unique filename
+            n += 1
+            # example: filename(0).zip
+            suffix = '('+str(n)+')'
 
 # Get the Key
 with open('filekey.key', 'rb') as f:
@@ -29,4 +51,16 @@ with open('filekey.key', 'rb') as f:
 
 fernet = Fernet(myKey)
 
-decrypt("filename.txt")
+myPath = input("Directory to your file (including final /):\n> ")
+fileName = input("The filename (including .zip) of your input file:\n> ")
+
+operation = input("Decrypt or Encrypt (d/e):\n> ")
+
+if operation == 'd':
+    decrypt(myPath, fileName)
+
+elif operation == 'e':
+    encrypt(myPath, fileName)
+
+else:
+    print('error')
