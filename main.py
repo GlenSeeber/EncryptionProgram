d8d3381da479ec422db1989f9f3cb928f25fb0cf
Glen Seeber <gseeber5@gmail.com>
2022-06-07 15:00:33 -0700

    add password functionality I copied a function from the fernet docs on how to derive a key from a passcode
    it uses randomly generated salt to create the key. We will be sending the salt along
    with the locked up file. The user must have the passcode in order to derive the key
    from the salt. This means that all security of this file is dependant upon the user
    keeping this password secret.
    
    Encrypt() -> generates random salt, saves it in salt.key, locks up an input file using
    that salt, along with the user inputted passcode.
    
    Decrypt() -> reads the salt.key file, asks for a passcode. It uses both of those in
    order to derive a key, and decrypt the file. If the password is wrong, it will still
    perform the action, you just won't get anything meaningful outputted.

--- main.py
+++ main.py
@@ -1,18 +1,72 @@
+import base64
+import os
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# take a password, and securely derive a random fernet key from it
+# taken from fernet docs: 
+# https://cryptography.io/en/latest/fernet/#using-passwords-with-fernet
+def passwordToKey(password, salt = b''):
 
-def encrypt(myPath, fileName):
-    #read a file
+    password = password.encode('ascii')
+
+    # if they don't pass salt in the function (or if they pass the wrong
+    # bit size of salt), generate new salt
+    if len(salt) != 16:
+        salt = os.urandom(16)
+
+    kdf = PBKDF2HMAC(
+
+        algorithm=hashes.SHA256(),
+
+        length=32,
+
+        salt=salt,
+
+        iterations=390000,
+    )
+
+    myKey = base64.urlsafe_b64encode(kdf.derive(password))
+
+    #you need to keep the salt if you want to be able to derive the same
+    #key from the password in the future (i.e. if you want to actually
+    # be able to use the password to unlock stuff)
+    return myKey, salt
+
+def encrypt(myPath, fileName, myPass):
+    # save key and salt from the password, don't save the password.
+    myKey, salt = passwordToKey(myPass)     # [debug] remember to add password
+
+    # save salt
+    with open('salt.key', 'wb') as f:
+        f.write(salt)
+
+    # use key for fernet
+    fernet = Fernet(myKey)
+
+    # open input file
     with open(myPath+fileName, 'rb') as f:
         inData = f.read()
 
+    # encrypt input data
     outData = fernet.encrypt(inData)
 
-    #create unique file
+    #create unique file, paste encrypted data onto it
     writeNewFile(myPath, 'encrypted-output', content=outData)
 
 
-def decrypt(myPath, fileName):
+def decrypt(myPath, fileName, myPass):
+    # find salt
+    with open('salt.key', 'rb') as f:
+        salt = f.read()
+
+    # find key using password and salt.
+    myKey = passwordToKey(myPass, salt)[0]     # [debug] remember to add password and salt
+
+    # use key for fernet
+    fernet = Fernet(myKey)
+
     #read a file
     with open(myPath+fileName, 'rb') as f:
         inData = f.read()
@@ -45,38 +99,19 @@ def writeNewFile(myPath, fileName, extension='.zip', flags='b', content=''):
             suffix = '('+str(n)+')'
 
 
-# Get the Key
-try:
-    # [debug] you could add a unit test here, it would remove the 
-    # content from filekey.key/delete it, and then allow the except to run
-    
-    with open('filekey.key', 'rb') as f:
-        myKey = f.read()
-    
-    if (len(myKey) <= 0):
-        raise
-#if the filekey.key is empty/doesn't exist, generate a new key
-except:
-    exec(open("generate.py").read())    #run our generate.py script
-    print('no encryption key was found, a new one has been generated')  #debug message
-
-    #actually open the file now that we did that, assign the value to myKey
-    with open('filekey.key', 'rb') as f:
-        myKey = f.read()
-
-
-fernet = Fernet(myKey)
 
 myPath = input("Directory to your file (including final /):\n> ")
 fileName = input("The filename (including .zip) of your input file:\n> ")
 
 operation = input("Decrypt or Encrypt (d/e):\n> ")
 
-if operation == 'd':
-    decrypt(myPath, fileName)
+password = input('passcode:\n> ')
 
-elif operation == 'e':
-    encrypt(myPath, fileName)
+if operation == 'e':
+    encrypt(myPath, fileName, password)
+
+elif operation == 'd':
+    decrypt(myPath, fileName, password)
 
 else:
     print('error')
