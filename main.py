2a480a7ad9ff1009b7af2a0ae388e9d0328df0fb
Glen Seeber <gseeber5@gmail.com>
2022-06-07 10:19:45 -0700

    add basic funcs

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('hello world')
+from cryptography.fernet import Fernet
+
+def encrypt():
+    #read a file
+    with open('stuff/filename.txt', 'rb') as f:
+        inData = f.read()
+
+    outData = fernet.encrypt(inData)
+
+    #write to a file
+    with open('stuff/filename.txt', 'wb') as f:
+        f.write(outData)
+
+def decrypt():
+    #read a file
+    with open('stuff/filename.txt', 'rb') as f:
+        inData = f.read()
+
+    outData = fernet.decrypt(inData)
+
+    #write to a file
+    with open('stuff/filename.txt', 'wb') as f:
+        f.write(outData)
+
+
+# Get the Key
+with open('filekey.key', 'rb') as f:
+    myKey = f.read()
+
+fernet = Fernet(myKey)
+
+decrypt()
